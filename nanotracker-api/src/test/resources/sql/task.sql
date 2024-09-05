ce18453729340b86df927ef5a864fae2247f293b
nanokulon <nanokulonq@users.noreply.github.com>
2024-09-05 18:54:49 +0300

    test: add integration test for TasksRestController

---
+++ nanotracker-api/src/test/resources/sql/task.sql
@@ -0,0 +1,5 @@
+INSERT INTO task_management.t_task (id, c_title, c_details, c_creation_date, c_completed, id_user)
+VALUES
+    (1, 'Задача 1', 'Описание задачи 1', NOW(), false, 1),
+    (2, 'Задача 2', 'Описание задачи 2', NOW(), false, 1),
+    (3, 'Задача 3', 'Описание задачи 3', NOW(), false, 1);
---
+++ nanotracker-api/src/test/resources/sql/user.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_management.t_user (id, c_email, c_username, c_password)
+VALUES
+    (1, 'testuser@example.com', 'testuser', 'testpassword');
