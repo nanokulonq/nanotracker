bc8b7c28b83e36acddd8951f399477095f1abde0
nanokulon <nanokulonq@users.noreply.github.com>
2024-09-07 13:38:07 +0300

    test: add integration test for TaskRestController

---
+++ nanotracker-api/src/test/java/com/nanokulon/nanotracker/controller/TaskRestControllerIT.java
@@ -0,0 +1,351 @@
+package com.nanokulon.nanotracker.controller;
+
+import com.nanokulon.nanotracker.security.TrackerUserDetails;
+import com.nanokulon.nanotracker.service.JwtService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Интеграционные тесты TaskRestController")
+class TaskRestControllerIT {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    private JwtService jwtService;
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void findTask_TaskExists_ReturnsTask() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.get("/api/v1/tasks/1")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON),
+                        content().json("""
+                                {"id": 1, "title": "Задача 1", "details": "Описание задачи 1", "isCompleted": false}
+                                """)
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void findTask_TaskDoesNotExist_ReturnsNotFound() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.get("/api/v1/tasks/5")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "detail": "Task not found"
+                                }""")
+                );
+    }
+
+    @Test
+    void findTask_UserIsNotAuthenticated_ReturnsUnauthorized() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.get("/api/v1/tasks/1");
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnauthorized()
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void findTask_TaskDoesNotBelongToUser_ReturnsForbidden() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(2, "testuser1@example.com",
+                "testuser1", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.get("/api/v1/tasks/1")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "detail": "Task doesn't belong to user"
+                                }""")
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void updateTask_RequestIsValid_ReturnsNoContent() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.patch("/api/v1/tasks/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "Название",
+                        "details": "Описание"
+                        }""")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNoContent()
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void updateTask_RequestIsInvalid_ReturnsBadRequest() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.patch("/api/v1/tasks/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "",
+                        "details": "Описание"
+                        }""")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "errors": [
+                                "Specify the task name",
+                                "The task title must be between 3 and 100 characters"
+                                ]}""")
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void updateTask_TaskDoesNotExist_ReturnsNotFound() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.patch("/api/v1/tasks/5")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "Название",
+                        "details": "Описание"
+                        }""")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "detail": "Task not found"
+                                }""")
+                );
+    }
+
+    @Test
+    void updateTask_UserIsNotAuthenticated_ReturnsUnauthorized() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.patch("/api/v1/tasks/5")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "Название",
+                        "details": "Описание"
+                        }""");
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnauthorized()
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void updateTask_TaskDoesNotBelongToUser_ReturnsForbidden() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(2, "testuser1@example.com",
+                "testuser1", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.patch("/api/v1/tasks/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"title": "Название",
+                        "details": "Описание"
+                        }""")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "detail": "Task doesn't belong to user"
+                                }""")
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void deleteTask_TaskExists_ReturnsNoContent() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.delete("/api/v1/tasks/1")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNoContent()
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void deleteTask_TaskDoesNotExist_ReturnsNotFound() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(1, "testuser@example.com",
+                "testuser", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.delete("/api/v1/tasks/5")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNotFound(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "detail": "Task not found"
+                                }""")
+                );
+    }
+
+    @Test
+    void deleteTask_UserIsNotAuthenticated_ReturnsUnauthorized() throws Exception {
+        // given
+        var requestBuilder = MockMvcRequestBuilders.delete("/api/v1/tasks/1");
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isUnauthorized()
+                );
+    }
+
+    @Test
+    @Sql("/sql/user.sql")
+    @Sql("/sql/task.sql")
+    void deleteTask_TaskDoesNotBelongToUser_ReturnsForbidden() throws Exception {
+        // given
+        UserDetails userDetails = new TrackerUserDetails(2, "testuser1@example.com",
+                "testuser1", "testpassword", Collections.emptyList());
+        String token = jwtService.generateToken(userDetails);
+
+        var requestBuilder = MockMvcRequestBuilders.delete("/api/v1/tasks/1")
+                .header("Authorization", "Bearer " + token);
+
+        // when
+        mockMvc.perform(requestBuilder)
+                // then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden(),
+                        content().contentType(MediaType.APPLICATION_PROBLEM_JSON),
+                        content().json("""
+                                {
+                                "detail": "Task doesn't belong to user"
+                                }""")
+                );
+    }
+}
--- nanotracker-api/src/test/resources/sql/user.sql
+++ nanotracker-api/src/test/resources/sql/user.sql
@@ -1,3 +1,4 @@
 INSERT INTO user_management.t_user (id, c_email, c_username, c_password)
 VALUES
-    (1, 'testuser@example.com', 'testuser', 'testpassword');
+    (1, 'testuser@example.com', 'testuser', 'testpassword'),
+    (2, 'testuser1@example.com', 'testuser1', 'testpassword');
