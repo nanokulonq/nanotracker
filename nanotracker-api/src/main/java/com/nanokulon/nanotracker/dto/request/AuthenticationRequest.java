f8fdee34c9728b98e604fdb293aa56dc7feab6bd
nanokulon <nanokulonq@users.noreply.github.com>
2024-09-07 17:20:46 +0300

    test: add unit tests for AuthenticationRestController

--- nanotracker-api/src/main/java/com/nanokulon/nanotracker/dto/request/AuthenticationRequest.java
+++ nanotracker-api/src/main/java/com/nanokulon/nanotracker/dto/request/AuthenticationRequest.java
@@ -1,9 +1,11 @@
 package com.nanokulon.nanotracker.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class AuthenticationRequest {
     @NotBlank(message = "{users.auth.errors.username_is_null}")
     private String username;
