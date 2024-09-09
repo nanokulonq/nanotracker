e76eeb64a1d99343b54352a5bd488c2d0aea43da
nanokulon <nanokulonq@users.noreply.github.com>
2024-09-09 19:41:13 +0300

    feat: add initial setup to add-task-scheduler branch

---
+++ nanotracker-scheduler/src/main/java/com/nanokulon/NanoTrackerSchedulerApplication.java
@@ -0,0 +1,11 @@
+package com.nanokulon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoTrackerSchedulerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NanoTrackerSchedulerApplication.class, args);
+    }
+}
